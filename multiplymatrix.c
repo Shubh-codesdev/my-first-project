2a11a5b970a189507489b3e0807565e4528a3c79
Shubh-codesdev <singhshubh414@gmail.com>
2026-09-22 15:27:12 +0530

    multiplication of matrix

---
+++ multiplymatrix.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    int a[2][3],b[2][3],c[2][3];
+    int i,j;
+
+    printf("enter 6 numbers for first matrix:\n ");
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+        printf("\nenter 6 numbers for second matrix:\n ");
+
+        for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+
+    }
+    printf("\n");
+     for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]*b[i][j];
+        }
+    }
+    printf("\n");
+    printf("The multiplication of 2 matrices is:\n ");
+     for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
